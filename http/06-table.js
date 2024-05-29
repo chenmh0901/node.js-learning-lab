05a78b78260a5d3880733c3167796993f14aa399
amumu <1210204146@zust.edu.cn>
2024-05-29 14:33:36 +0800

    update http

--- http/06-table.js
+++ http/06-table.js
@@ -2,17 +2,40 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const MIME_TYPE = {
+  'html': 'text/html',
+  'css': 'text/css',
+  'js': 'text/javascript',
+  'png': 'image/png',
+  'jpg': 'image/jpeg',
+  'jpeg': 'image/jpeg',
+  'gif': 'image/gif',
+  'ico': 'image/x-icon',
+};
 const server = http.createServer((request, response) => {
   const { pathname } = new URL(request.url, `http://${request.headers.host}`);
   const filePath = __dirname + '/06-table' + pathname;
+  const ext = path.extname(filePath).slice(1);
   console.log('filePath:', filePath);
+  console.log('ext:', ext);
   fs.readFile(filePath, (error, data) => {
     if (error) {
-      response.statusCode = 500;
-      response.end('读取失败')
+      console.log(error)
+      switch (error.code) {
+        case 'ENOENT':
+          response.statusCode = 404;
+          response.end('<h1>404 Not Found</h1>');
+          break;
+      }
       return;
     }
-    response.end(data);
+    if (ext === 'html') {
+      response.setHeader('Content-Type', MIME_TYPE[ext] + ';charset=utf-8');
+      response.end(data);
+    } else {
+      response.setHeader('Content-Type', MIME_TYPE[ext]);
+      response.end(data);
+    }
   });
 }
 );
--- http/http.md
+++ http/http.md
@@ -5,3 +5,6 @@
 - 端口：实现不同主机之间的应用程序的通信 http:80 https:443 资源监视器查看端口占用 PID。
 - response.setHeader('Content-Type', 'text/html;charset=utf-8');返回内容允许中文。
 - response.end()只允许有一个。
+- /url:绝对路径 完整、/url（与协议、IP、端口拼接） ./url:相对路径。与当前页面路径进行计算。
+- MIME：文件类型。response.setHeader('Content-Type', 'text/html;charset=utf-8'); text/html text/css text/javascript image/jpeg image/png image/gif image/svg+xml application/json application/xml；
+- html meta=utf-8 设置网页字符集包含汉字。
